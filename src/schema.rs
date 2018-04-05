37a0a3e25a875c9169d7627e3e6a0569873340f7
LunaFrisch <35052802+LunaFrisch@users.noreply.github.com>
2018-04-05 02:14:07 +0000

    All these small mistakes are making me hungry. x.x

--- src/schema.rs
+++ src/schema.rs
@@ -1,1 +1,64 @@
-Diesel only supports tables with primary keys. Table packages has no primary key
+table! {
+    packages (userid) {
+        userid -> Int4,
+        packages -> Text,
+    }
+}
+
+table! {
+    profiles (userid) {
+        userid -> Int4,
+        name -> Text,
+        role -> Text,
+        favor -> Int4,
+        totalclients -> Int4,
+        totalvisits -> Int4,
+    }
+}
+
+table! {
+    purchases (userid) {
+        userid -> Int4,
+        gelbooru -> Bool,
+        rule34 -> Bool,
+        e621 -> Bool,
+        yandere -> Bool,
+        lmgtfy -> Bool,
+    }
+}
+
+table! {
+    roles (rolename) {
+        rolename -> Text,
+        tip -> Bool,
+        clockin -> Bool,
+        clockout -> Bool,
+        confirm -> Bool,
+        deny -> Bool,
+        list -> Bool,
+        offer -> Bool,
+        feedback -> Bool,
+        gelbooru -> Bool,
+        rule34 -> Bool,
+        e621 -> Bool,
+        yandere -> Bool,
+        urban -> Bool,
+        lmgtfy -> Bool,
+        checkfavor -> Bool,
+        setrole -> Bool,
+        setworker -> Bool,
+        removeworker -> Bool,
+        blacklist -> Bool,
+        say -> Bool,
+        checkfeedback -> Bool,
+        setpackage -> Bool,
+        profile -> Bool,
+    }
+}
+
+allow_tables_to_appear_in_same_query!(
+    packages,
+    profiles,
+    purchases,
+    roles,
+);
